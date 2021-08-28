88ad28f630ab40bbff905cae9abff0f761c8a40b
Rajkumar N <rajkumar.natarajan@gmail.com>
2021-08-28 12:28:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,110 @@
+import sqlite3
+import logging
+from datetime import datetime
+
+from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from werkzeug.exceptions import abort
+
+
+# Function to get a database connection.
+# This function connects to database with the name `database.db`
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+# Function to get a post using its ID
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                              (post_id,)).fetchone()
+    connection.close()
+    return post
+
+
+# Function to return metrics
+def get_metrics():
+	connection = get_db_connection()
+	count = connection.execute('SELECT count(*) FROM posts').fetchone()
+	connection.close()
+	print(count[0])
+	return dict({'db_connection_count': '1', 'post_count': count[0]})
+
+
+# Define the Flask application
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+logging.basicConfig(level=logging.DEBUG)
+logging.Formatter(fmt='%(levelname)s %(funcName)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)12s, %(message)s')
+#logger = logging.basicConfig(level=logging.DEBUG, format='%(asctime)12s, %(message)s')
+
+
+# Define the main route of the web application
+@app.route('/')
+def index():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
+
+
+# Define how each individual article is rendered
+# If the post ID is not found a 404 page is shown
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    if post is None:
+        return render_template('404.html'), 404
+    else:
+        # app.logger.info('Article %s retrieved! ', post['title'])
+        # app.logger.error('Hello World')
+        # print('Hello World {} '.format(post['title']))
+        app.logger.info('%s Article "%s" retrieved! ', datetime.now().strftime("%d/%m/%Y, %H:%M:%S,"), post['title'])
+        return render_template('post.html', post=post)
+
+
+# Define the About Us page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# Define the health checker 
+@app.route('/healthz')
+def healthz():
+	return json.dumps(dict({'result': 'Ok - healthy'}))
+
+
+# Define the metrics 
+@app.route('/metrics')
+def metrics():
+	metric = get_metrics() 
+	return json.dumps(metric)
+
+
+# Define the post creation functionality
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            connection = get_db_connection()
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                               (title, content))
+            connection.commit()
+            connection.close()
+
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+# start the application on port 3111
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='3111')
